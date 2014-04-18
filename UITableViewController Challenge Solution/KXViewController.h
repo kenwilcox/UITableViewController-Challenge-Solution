cbe57e52fee399e648cf59b7a5a5b2d1bd0dfe5d
kenwilcox <wilcoxk@gmail.com>
2014-04-17 22:17:20 -0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/KXViewController.h
@@ -0,0 +1,13 @@
+//
+//  KXViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Kenneth Wilcox on 4/17/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KXViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Kenneth Wilcox on 4/17/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([KXAppDelegate class]));
+  }
+}
